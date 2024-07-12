b6414c4285f4362f435996f3b017ffd9190cf615
Rudolf Hersén <rudolf.hersen@schibsted.com>
2024-07-12 12:59:09 +0200

    static

---
+++ src/routes/+page.server.js
@@ -0,0 +1,86 @@
+import { error } from '@sveltejs/kit';
+
+async function positionResult() {
+	const response = await fetch('https://api.trafikinfo.trafikverket.se/v2/data.json', {
+		method: 'POST',
+		body: positionQuery(),
+		headers: {
+			'Content-Type': 'application/xml',
+			Accept: 'application/json'
+		}
+	});
+
+	if (!response.ok) throw error(response.status, response.statusText);
+
+	const { RESPONSE } = await response.json();
+	const [result] = RESPONSE.RESULT;
+	return { positions: result.TrainPosition, ssePosition: result.INFO?.SSEURL };
+}
+
+async function announcementResult(set) {
+	const response = await fetch('https://api.trafikinfo.trafikverket.se/v2/data.json', {
+		method: 'POST',
+		body: announcementQuery(Array.from(set)),
+		headers: {
+			'Content-Type': 'application/xml',
+			Accept: 'application/json'
+		}
+	});
+
+	if (!response.ok) throw error(response.status, response.statusText);
+
+	const { RESPONSE } = await response.json();
+	const [result] = RESPONSE.RESULT;
+	return { announcements: result.TrainAnnouncement, sseAnnouncement: result.INFO?.SSEURL };
+}
+
+export const load = async () => {
+	const { positions, ssePosition } = await positionResult();
+	const { announcements, sseAnnouncement } = await announcementResult(
+		new Set(positions.map(({ Train }) => Train.AdvertisedTrainNumber))
+	);
+	return { positions, announcements, ssePosition, sseAnnouncement };
+};
+
+const minutes = 6e4;
+
+function positionQuery() {
+	const since = new Date(Date.now() - 3 * minutes).toISOString();
+	return `
+<REQUEST>
+  <LOGIN authenticationkey='${process.env.TRAFIKVERKET_API_KEY}' />
+    <QUERY objecttype='TrainPosition' namespace='järnväg.trafikinfo' sseurl='false' schemaversion='1.1'>
+    <FILTER>
+      <GT name='TimeStamp' value='${since}'/>
+      <WITHIN name="Position.SWEREF99TM" shape="box" value="553946 6493105, 679983 6659735"/>
+    </FILTER>
+    <INCLUDE>Bearing</INCLUDE>
+    <INCLUDE>Position</INCLUDE>
+    <INCLUDE>Speed</INCLUDE>
+    <INCLUDE>TimeStamp</INCLUDE>
+    <INCLUDE>Train</INCLUDE>
+  </QUERY>
+</REQUEST>`;
+}
+
+function announcementQuery(idArray) {
+	const since = new Date(Date.now() - 15 * minutes).toISOString();
+	return `
+<REQUEST>
+  <LOGIN authenticationkey='${process.env.TRAFIKVERKET_API_KEY}' />
+    <QUERY objecttype='TrainAnnouncement' orderby='AdvertisedTimeAtLocation' sseurl='false' schemaversion='1.6'>
+      <FILTER>
+        <IN name='AdvertisedTrainIdent' value='${idArray}' />
+        <GT name='TimeAtLocationWithSeconds' value='${since}' />
+        <EXISTS name='ToLocation' value='true' />
+      </FILTER>
+      <INCLUDE>ActivityType</INCLUDE>
+      <INCLUDE>LocationSignature</INCLUDE>
+      <INCLUDE>TimeAtLocationWithSeconds</INCLUDE>
+      <INCLUDE>AdvertisedTrainIdent</INCLUDE>
+      <INCLUDE>FromLocation</INCLUDE>
+      <INCLUDE>ProductInformation</INCLUDE>
+      <INCLUDE>ToLocation</INCLUDE>
+    </QUERY>
+</REQUEST>`;
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,51 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount, onDestroy } from 'svelte';
+
+	let mapElement;
+	let map;
+
+	export let data;
+
+	onMount(async () => {
+		const leaflet = await import('leaflet');
+
+		map = leaflet.map(mapElement).setView([59.34389933923258, 17.053451499025947], 9);
+
+		leaflet
+			.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+				attribution:
+					'© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+			})
+			.addTo(map);
+
+		data.positions.forEach((position) => {
+			const [lat, lon] = wgs84(position.Position.WGS84);
+			leaflet.marker([lon, lat]).addTo(map).bindPopup(position.Train.AdvertisedTrainNumber);
+		});
+	});
+
+	onDestroy(async () => {
+		if (map) {
+			console.log('Unloading Leaflet map.');
+			map.remove();
+		}
+	});
+
+	function wgs84(s) {
+		return s
+			.match(/POINT \(([^ ]+) ([^ ]+)\)/)
+			.slice(1)
+			.map(Number);
+	}
+</script>
+
+<main>
+	<div bind:this={mapElement}></div>
+</main>
+
+<style>
+	@import 'leaflet/dist/leaflet.css';
+	main div {
+		height: 800px;
+	}
+</style>

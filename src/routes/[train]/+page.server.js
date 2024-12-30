28a8652f9029deef8fb18777df00dc59f51954f3
rudolf.hersen@schibsted.se <rudolf.hersen@schibsted.com>
2024-12-30 14:01:37 +0100

    train id as path param

--- src/routes/[train]/+page.server.js
+++ src/routes/[train]/+page.server.js
@@ -1,9 +1,9 @@
 import { error } from '@sveltejs/kit';
 
-async function positionResult() {
+async function positionResult(train) {
 	const response = await fetch('https://api.trafikinfo.trafikverket.se/v2/data.json', {
 		method: 'POST',
-		body: positionQuery(),
+		body: positionQuery(train),
 		headers: {
 			'Content-Type': 'application/xml',
 			Accept: 'application/json'
@@ -17,10 +17,10 @@ async function positionResult() {
 	return { positions: result.TrainPosition, ssePosition: result.INFO?.SSEURL };
 }
 
-async function announcementResult(set) {
+async function announcementResult(train) {
 	const response = await fetch('https://api.trafikinfo.trafikverket.se/v2/data.json', {
 		method: 'POST',
-		body: announcementQuery(Array.from(set)),
+		body: announcementQuery(train),
 		headers: {
 			'Content-Type': 'application/xml',
 			Accept: 'application/json'
@@ -34,17 +34,15 @@ async function announcementResult(set) {
 	return { announcements: result.TrainAnnouncement, sseAnnouncement: result.INFO?.SSEURL };
 }
 
-export const load = async () => {
-	const { positions, ssePosition } = await positionResult();
-	const { announcements, sseAnnouncement } = await announcementResult(
-		new Set(positions.map(({ Train }) => Train.AdvertisedTrainNumber))
-	);
+export const load = async ({ params }) => {
+	const { positions, ssePosition } = await positionResult(params.train);
+	const { announcements, sseAnnouncement } = await announcementResult(params.train);
 	return { positions, announcements, ssePosition, sseAnnouncement };
 };
 
 const minutes = 6e4;
 
-function positionQuery() {
+function positionQuery(train) {
 	const since = new Date(Date.now() - 3 * minutes).toISOString();
 	return `
 <REQUEST>
@@ -52,7 +50,7 @@ function positionQuery() {
     <QUERY objecttype='TrainPosition' namespace='järnväg.trafikinfo' sseurl='true' schemaversion='1.1'>
     <FILTER>
       <GT name='TimeStamp' value='${since}'/>
-      <EQ name="Train.AdvertisedTrainNumber" value="2757"/>
+      <EQ name="Train.AdvertisedTrainNumber" value='${train}'/>
     </FILTER>
     <INCLUDE>Bearing</INCLUDE>
     <INCLUDE>Position</INCLUDE>

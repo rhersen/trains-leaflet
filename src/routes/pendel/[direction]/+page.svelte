8614b284f41d7bd547b2b501f491a93f6df91df5
rudolf.hersen@schibsted.se <rudolf.hersen@schibsted.com>
2025-02-07 10:22:59 +0100

    always create new icons

--- src/routes/pendel/[direction]/+page.svelte
+++ src/routes/pendel/[direction]/+page.svelte
@@ -21,17 +21,21 @@
 			popupAnchor: [0, -16]
 		};
 
-		function createCircleIcon(hue) {
-			const black = `<circle cx="32" cy="32" r="25" fill="none" stroke="black" stroke-width="12" />`;
-			const circle = `<circle cx="32" cy="32" r="25" fill="none" stroke="hsl(${hue} 100% 50%" stroke-width="8" />`;
-			const b = `<svg width="64" height="64" xmlns="http://www.w3.org/2000/svg">${black}${circle}</svg>`;
-			return L.icon({
-				...iconSize,
-				iconUrl: `data:image/svg+xml;base64,${btoa(b)}`
-			});
-		}
+		function icon(trainNumber) {
+			const d = differenceInSeconds(
+				announcements[trainNumber]?.TimeAtLocationWithSeconds,
+				announcements[trainNumber]?.AdvertisedTimeAtLocation
+			);
 
-		function createSquareIcon(hue) {
+			let hue;
+			if (isNaN(d)) hue = -1;
+			else if (d < 120) hue = 120;
+			else if (d < 180) hue = 75;
+			else if (d < 300) hue = 60;
+			else if (d < 600) hue = 33;
+			else if (d < 900) hue = 25;
+			else hue = 0;
+
 			const black = `<polygon points="32,8 56,32 32,56 8,32" fill="none" stroke="black" stroke-width="12" />`;
 			const diamond = `<polygon points="32,8 56,32 32,56 8,32" fill="none" stroke="hsl(${hue} ${hue === -1 ? '0%' : '100%'} 50%)" stroke-width="8" />`;
 			const b = `<svg width="64" height="64" xmlns="http://www.w3.org/2000/svg">${black}${diamond}</svg>`;
@@ -39,36 +43,6 @@
 				...iconSize,
 				iconUrl: `data:image/svg+xml;base64,${btoa(b)}`
 			});
-		}
-
-		const circle0 = createCircleIcon(120);
-		const circle1 = createCircleIcon(75);
-		const circle3 = createCircleIcon(60);
-		const circle5 = createCircleIcon(33);
-		const circle10 = createCircleIcon(25);
-		const circle15 = createCircleIcon(0);
-
-		const squareNaN = createSquareIcon(-1);
-		const square0 = createSquareIcon(120);
-		const square1 = createSquareIcon(75);
-		const square3 = createSquareIcon(60);
-		const square5 = createSquareIcon(33);
-		const square10 = createSquareIcon(25);
-		const square15 = createSquareIcon(0);
-
-		function icon(trainNumber) {
-			const d = differenceInSeconds(
-				announcements[trainNumber]?.TimeAtLocationWithSeconds,
-				announcements[trainNumber]?.AdvertisedTimeAtLocation
-			);
-			const atStation = announcements[trainNumber]?.ActivityType === 'Ankomst';
-			if (isNaN(d)) return squareNaN;
-			if (d < 120) return atStation ? square0 : circle0;
-			if (d < 180) return atStation ? square1 : circle1;
-			if (d < 300) return atStation ? square3 : circle3;
-			if (d < 600) return atStation ? square5 : circle5;
-			if (d < 900) return atStation ? square10 : circle10;
-			return atStation ? square15 : circle15;
 		}
 
 		map = L.map(mapElement).setView([59.33, 18.07], 11);
